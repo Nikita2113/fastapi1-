ccd0608da24e83c05e1aa2c6bc5f81fb948cf2d2
Nikita2113 <nikita_sergeev34@mail.ru>
2026-02-27 18:38:46 +0400

    fix: template in base

--- fastapi_app/src/api/base.py
+++ fastapi_app/src/api/base.py
@@ -13,7 +13,7 @@ async def get_hello_world() -> dict:
     return response
 
 
-@router.post("/test_json", status_code=status.HTTP_201_CREATED, response_model=PostBase)
+@router.post("/test_json", status_code=status.HTTP_201_CREATED, response_model=Post)
 async def test_json(post: Post) -> dict:
     if len(post.text) < 3:
         raise HTTPException(
@@ -22,8 +22,26 @@ async def test_json(post: Post) -> dict:
         )
 
     response = {
-        "post_text": post.text,
-        "author_name": post.author.login
-    }
+  "id": 0,
+  "author": {
+    "login": "string",
+    "id": 0,
+    "created_at": "2026-02-27T14:28:49.181Z"
+  },
+  "category": {
+    "id": 0,
+    "author": {
+      "login": "string",
+      "id": 0,
+      "created_at": "2026-02-27T14:28:49.181Z"
+    },
+    "title": "string",
+    "description": "string",
+    "is_published": 'true'
+  },
+  "text": "string",
+  "datetime_to_publish": "2026-02-27T14:28:49.181Z",
+  "created_at": "2026-02-27T14:28:49.181Z"
+}
 
     return Post.model_validate(obj=response)
